cda14d52d2eab7811d035c213b93d0f1651c0417
udexmickey <jerrymike80@yahoo.com>
2020-09-10 19:29:11 +0100

    added the the header text and opacity to the background

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 // alert("really");
+const headH1 = document.querySelector(".headH1");
+const headColor = document.querySelector(".headColor");
+
+headH1.addEventListener("click", function(){
+    // headColor.style.color = "#fff";
+    console.log("its working");
+});
