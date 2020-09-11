4a02f36861df5e2b7361fba6ebc52772d128bc90
udexmickey <jerrymike80@yahoo.com>
2020-09-11 07:32:44 +0100

    added the manu page fixed 100% by the right corner and still manupulating around the hamburger manu

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 // alert("really");
 const headH1 = document.querySelector(".headH1");
 const headColor = document.querySelector(".headColor");
+const fixedMenu = document.querySelector(".fixedMenu");
+const slipt = document.querySelector(".slipt");
+const checkBox = document.querySelector("input'[type=checkbox]'");
 
 headH1.addEventListener("click", function(){
     // headColor.style.color = "#fff";
     console.log("its working");
+});
+
+function getMenu(){
+    fixedMenu.style.display = "block";
+    // checkBox.toggle(fixedMenu);
+}
+checkBox.addEventListener("click", function(){
+    getMenu();
 });
